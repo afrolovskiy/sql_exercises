f6f25a00dfa4df126166262f7510a04b558f86e2
alex <alex@alex-HP-Pavilion-dv5-Notebook-PC.(none)>
2012-10-28 20:03:22 +0400

    add query from 6 to 8

--- scripts.sql
+++ scripts.sql
@@ -33,3 +33,41 @@ JOIN Movie m ON ra.mID = m.mID
 JOIN Reviewer re ON ra.rID = re.rID
 ORDER BY name, title, stars
 LIMIT 4; 
+
+/*6) Для всех случаев, когда один эксперт оценивал фильм дважды и указал лучший 
+  рейтинг второй раз, выведите имя эксперта и навание фильма*/
+SELECT name, title
+FROM (
+	SELECT rID, mID, max(stars) as max_stars, max(ratingDate) as max_ratingDate
+	FROM Rating
+	GROUP BY rID
+	HAVING count(rID) = 2
+) as tmp 
+JOIN Rating ra ON ra.rID = tmp.rID and ra.mID = tmp.mID
+JOIN Movie m ON m.mID = tmp.mID
+JOIN Reviewer re ON re.rID = tmp.rID
+WHERE stars = max_stars AND ratingDate = max_ratingDate;
+
+/*7) Для каждого фильма, который имеет, по крайней мере, одну оценку, найти 
+  наибольшее количество звезд, что фильм получил. Выбрать название фильма и
+  количество звезд. Сортировать по названию фильма.*/
+SELECT title, max(stars)
+FROM Rating ra
+JOIN Movie m ON ra.mID = m.mID
+GROUP BY ra.mID
+ORDER BY title;
+
+/*8) Для каждого фильма выбрать название и "разброс оценок", то есть разницу между 
+  самой высокой и самой низкой оценками для этого фильма. Сортировать по "разбросу
+  оценок" от высшего к низшему и по названию фильма */
+SELECT title,  max(stars) -  min(stars) as diff_stars
+FROM Rating r
+JOIN Movie m ON r.mID = m.mID
+GROUP BY r.mID
+ORDER BY diff_stars DESC, title;
+
+
+
+
+
+
